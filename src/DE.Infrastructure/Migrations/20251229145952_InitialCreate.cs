bef6147ffbd56894e869806339528d929e92058e
davidreyg <neisserrey@upeu.edu.pe>
2025-12-29 11:34:06 -0500

    fix: 🐛 Fix EF Core Mgirations (#12)
    * chore: :globe_with_meridians: update cspell.json
    
    * fix: ✨ Add initial EF Core migration for IdentityDocumentType entity
    
    * feat: ✨ Add VSCode tasks for EF Core migrations and application run commands

---
+++ src/DE.Infrastructure/Migrations/20251229145952_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DE.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IdentityDocumentType",
+                columns: table => new
+                {
+                    Id = table
+                        .Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(
+                        type: "nvarchar(10)",
+                        maxLength: 10,
+                        nullable: false
+                    ),
+                    Description = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    GuidCode = table.Column<Guid>(
+                        type: "uniqueidentifier",
+                        nullable: false,
+                        defaultValueSql: "NEWID()"
+                    ),
+                    Status = table.Column<int>(type: "int", nullable: false, defaultValue: 1),
+                    IsDeleted = table.Column<bool>(
+                        type: "bit",
+                        nullable: false,
+                        defaultValue: false
+                    ),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentityDocumentType", x => x.Id);
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "IdentityDocumentType");
+        }
+    }
+}
