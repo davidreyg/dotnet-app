05153d6eee66fdf1ac77502cfc44b964b1a4db62
David Rey <neisserrey@upeu.edu.pe>
2026-01-07 17:48:07 +0000

    feat: ✨ Add Ups entity, repository, and service implementation

---
+++ src/DE.Application/DTOs/Request/UpsRequest.cs
@@ -0,0 +1,7 @@
+namespace DE.Application.DTOs.Request;
+
+public class UpsRequest
+{
+    public long Id { get; set; } = default!;
+    public string Descripcion { get; set; } = default!;
+}
---
+++ src/DE.Application/DTOs/Response/UpsResponse.cs
@@ -0,0 +1,7 @@
+namespace DE.Application.DTOs.Response;
+
+public class UpsResponse
+{
+    public long Id { get; set; } = default!;
+    public string Descripcion { get; set; } = default!;
+}
