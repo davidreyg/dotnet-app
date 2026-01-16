d20b5c1669018c1cbada33837f00406e66980e51
David Rey <neisserrey@upeu.edu.pe>
2026-01-16 14:36:10 +0000

    feat ✨ Add Tariff entity with configuration and migration

---
+++ src/DE.Infrastructure/Migrations/20260116143448_Tariff.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DE.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tariff : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tariffs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cpms = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IILevel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IIILevel = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tariffs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tariffs");
+        }
+    }
+}
