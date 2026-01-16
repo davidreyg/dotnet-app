83b6fba4bd92fd97a44419113b575a0ab2c044c2
David Rey <neisserrey@upeu.edu.pe>
2026-01-16 20:44:36 +0000

    fix: :bug: Update ProfessionalCouncil entity: modify Description max length and adjust Code check constraint

---
+++ src/DE.Infrastructure/Migrations/20260116204320_FixProfessionalCouncilCode.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DE.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixProfessionalCouncilCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_ProfessionalCouncil_Code_Positive",
+                table: "ProfessionalCouncils");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ProfessionalCouncils",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_ProfessionalCouncil_Code_Positive",
+                table: "ProfessionalCouncils",
+                sql: "[Code] > -1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_ProfessionalCouncil_Code_Positive",
+                table: "ProfessionalCouncils");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ProfessionalCouncils",
+                type: "nvarchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_ProfessionalCouncil_Code_Positive",
+                table: "ProfessionalCouncils",
+                sql: "[Code] > 0");
+        }
+    }
+}
