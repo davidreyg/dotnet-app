468ca479c53427bb13dcf1f01402b6066debf435
David Rey <neisserrey@upeu.edu.pe>
2026-01-19 19:35:14 +0000

    feat: ✨ Add 'Code' column to Employees table and update model snapshot

--- src/DE.Infrastructure/Migrations/20260119191753_EmployeeAddCode.Designer.cs
+++ src/DE.Infrastructure/Migrations/20260119191753_EmployeeAddCode.Designer.cs
@@ -4,6 +4,7 @@ using DE.Infrastructure.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DE.Infrastructure.Migrations
 {
     [DbContext(typeof(DbContextApp))]
-    partial class DbContextAppModelSnapshot : ModelSnapshot
+    [Migration("20260119191753_EmployeeAddCode")]
+    partial class EmployeeAddCode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
